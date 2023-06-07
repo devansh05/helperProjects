e343dc3a6d228112f5a3d09fb2e119bae97f6e4f
devansh2393@gmail.com <devansh2393@gmail.com>
2023-06-07 16:47:33 +0530

    One to Few Mongo

---
+++ Mongo Relationships/Models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+//Initiating db connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/relationshipDemo")
+  .then(() => {
+    console.log("Mongo Connected!");
+  })
+  .catch((error) => {
+    console.log("Mongo Connection Error ", error);
+  });
+
+  const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    address: [
+        {
+            city: String,
+            street: String,
+            state: String
+        }
+    ]
+  })
+
+  const User = mongoose.model('User', userSchema);
+
+  const makeUser = async() => {
+    const u = new User({
+        first: 'Harry',
+        last: 'Potter',
+        address: [
+            {
+                street: '100 St',
+                city: 'Pune',
+                state: 'Maharashtra'
+            }
+        ]
+    })
+    u.address.push({
+        street: 'St 120',
+        city: 'New York',
+        state: 'New York'
+
+    });
+    const res = await u.save()
+    console.log('LOG res  ',res)
+  }
+
+  makeUser();
