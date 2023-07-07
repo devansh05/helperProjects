c7f496ac261db9c5439c92153005b7501412cc98
devansh2393@gmail.com <devansh2393@gmail.com>
2023-07-07 14:28:13 +0530

    Cookies and signing cookies

--- RouterDemo/index.js
+++ RouterDemo/index.js
@@ -3,10 +3,32 @@ const app = express();
 const dashboardRoutes = require('./routes/dashboardRoutes');
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+const cookieParser = require('cookie-parser');
 
+app.use(cookieParser('secretKey'));
 app.use('/dashboard', dashboardRoutes);
 app.use('/user', dashboardRoutes);
 app.use('/admin', adminRoutes);
+
+app.get('/greet', (req, res) => {
+    const { name } = req.cookies;
+    res.send(` Hi ${name}`)
+});
+
+app.get('/checkCookies', (req, res) => {
+    res.cookie('name', 'Hi cookie');
+    res.send('Cookie Sent with request always')
+})
+
+app.get('/signCookies', (req, res) => {
+    res.cookie('signedName', 'signedCookieValue', { signed : true});
+    res.send('Sign Cookies')
+})
+
+app.get('/verifyCookies', (req, res) => {
+    const { signed } = req.signedCookies;
+    res.send(signed)
+})
 
 app.listen(3000, () => {
     console.log('LOG Listening on 3000  ',)
