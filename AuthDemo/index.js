5bebdcf7051c55eac4b1c07b58d9093f35698b61
devansh2393@gmail.com <devansh2393@gmail.com>
2023-07-13 17:04:55 +0530

    Comparing login details and password

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -20,31 +20,53 @@ app.set('view engine', 'ejs');
 //set views directory of ejs to our views directory
 app.set('views', 'views');
 
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/register', async(req, res) => {
-    res.render('register');
+app.get('/register', async (req, res) => {
+  res.render('register');
+})
+
+app.get('/login', async (req, res) => {
+  res.render('login');
+})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  console.log('LOG username ', username)
+  const user = await User.findOne({ username });
+  console.log('LOG user ', user)
+  if (user) {
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (isValidPassword) {
+      res.redirect('/')
+    } else {
+      res.send('Invalid username or password.');
+    }
+  } else {
+    res.send('Invalid username or password.');
+  }
+
 })
 
 app.get('/secret', (req, res) => {
-    res.send('Secret Key')
+  res.send('Secret Key')
 });
 
 app.get('/', (req, res) => {
-    res.send('Home Page');
+  res.send('Home Page');
 })
 
-app.post('/register', async(req, res) => {
-    const { password, username } = req.body;
-    const hash = await bcrypt.hash( password, 12 );
-    const user = new User({
-        username,
-        password : hash
-    })
-    await user.save()
-    res.redirect('/')
+app.post('/register', async (req, res) => {
+  const { password, username } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hash
+  })
+  await user.save()
+  res.redirect('/')
 })
 
 app.listen(3000, () => {
-    console.log('Listening on 3000',)
+  console.log('Listening on 3000',)
 })
